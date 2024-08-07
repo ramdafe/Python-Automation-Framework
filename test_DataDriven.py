9d76191cae63cd2e6746b18069b44d0cfce82ad8
ramdafe <dafe.ram@gmail.com>
2024-08-07 19:35:34 +0530

    Initial commit

--- test_DataDriven.py
+++ test_DataDriven.py
@@ -1,12 +1,6 @@
 import pytest
 import openpyxl
 
-def test_create_auth_with_excel():
-    file_path = r"C:\Users\ram.dafe\Python312\Selenium With Python\Python Automation Framework\Sample_Excel_File.xlsx"
-    credentials = read_credentials_from_excel(file_path = file_path)
-    print("Type of credentials:", type(credentials))
-    print(credentials)
-
 def read_credentials_from_excel(file_path):
     credentials = []
     workbook = openpyxl.load_workbook(filename = file_path)
